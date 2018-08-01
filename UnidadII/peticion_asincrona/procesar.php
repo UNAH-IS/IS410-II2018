8f26b3f2e55d7c76d2ffe0d4fdc208bcb8d201c5
Erick Marín <erick.marin.v@gmail.com>
2018-08-01 09:52:38 -0600

    Peticion asincrona

--- UnidadII/peticion_asincrona/procesar.php
+++ UnidadII/peticion_asincrona/procesar.php
@@ -1,4 +1,13 @@
 <?php
-    sleep(10);
-    echo "<b>Usuario: " . $_GET["usuario"] . ", password: " . $_GET["password"]."</b>";
+    //sleep(10);
+    if ($_GET["respuesta"]=='html'){
+        echo "<b>Usuario: " . $_GET["usuario"] . ", password: " . $_GET["password"]."</b>";
+    }
+    else if ($_GET["respuesta"]=='json'){
+        $respuesta["usuario"] = $_GET["usuario"];
+        $respuesta["password"] =  $_GET["password"];
+
+        echo json_encode($respuesta); //Convierte un arreglo en json
+    }
+        
 ?>    
